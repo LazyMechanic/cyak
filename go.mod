35618f13a10eac6126daea0d473fab61c5885d4b
LazyMechanic <AsharnRus@gmail.com>
2020-02-07 19:43:39 +0300

    Adds targets package

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/LazyMechanic/cyak
 
 go 1.13
 
-require github.com/urfave/cli/v2 v2.1.1
+require (
+	github.com/AlecAivazis/survey/v2 v2.0.5
+	github.com/urfave/cli/v2 v2.1.1
+)
---
+++ targets/targets.go
@@ -0,0 +1,62 @@
+package targets
+
+type Project struct {
+	Name                 string `survey:"name"`
+	Lang                 string `survey:"lang"`
+	Namespace            string `survey:"namespace"`
+	LangStandard         string `survey:"standard"`
+	LangExtensions       string `survey:"extensions"`
+	LangStandardRequired string `survey:"standardrequired"`
+	MajorVersion         string `survey:"major"`
+	MinorVersion         string `survey:"minor"`
+	PatchVersion         string `survey:"patch"`
+}
+
+type Executable struct {
+	Name                 string `survey:"name"`
+	Lang                 string `survey:"lang"`
+	LangStandard         string `survey:"standard"`
+	LangExtensions       string `survey:"extensions"`
+	LangStandardRequired string `survey:"standardrequired"`
+	MajorVersion         string `survey:"major"`
+	MinorVersion         string `survey:"minor"`
+	PatchVersion         string `survey:"patch"`
+	HasTest              bool   `survey:"hastest"`
+	IsInherited          bool   `survey:"inherit"`
+}
+
+type Library struct {
+	Name                 string `survey:"name"`
+	Lang                 string `survey:"lang"`
+	Namespace            string `survey:"namespace"`
+	LangStandard         string `survey:"standard"`
+	LangExtensions       string `survey:"extensions"`
+	LangStandardRequired string `survey:"standardrequired"`
+	MajorVersion         string `survey:"major"`
+	MinorVersion         string `survey:"minor"`
+	PatchVersion         string `survey:"patch"`
+	HasTest              bool   `survey:"hastest"`
+	IsInherited          bool   `survey:"inherit"`
+}
+
+type Interface struct {
+	Name                 string `survey:"name"`
+	Lang                 string `survey:"lang"`
+	Namespace            string `survey:"namespace"`
+	LangStandard         string `survey:"standard"`
+	LangExtensions       string `survey:"extensions"`
+	LangStandardRequired string `survey:"standardrequired"`
+	MajorVersion         string `survey:"major"`
+	MinorVersion         string `survey:"minor"`
+	PatchVersion         string `survey:"patch"`
+	HasTest              bool   `survey:"hastest"`
+	IsInherited          bool   `survey:"inherit"`
+}
+
+type Test struct {
+	Name                 string `survey:"name"`
+	Lang                 string `survey:"lang"`
+	LangStandard         string `survey:"standard"`
+	LangExtensions       string `survey:"extensions"`
+	LangStandardRequired string `survey:"standardrequired"`
+}
