25bdb8a6f31254d312da368d97202e0158497307
LazyMechanic <AsharnRus@gmail.com>
2020-07-14 19:51:45 +0300

    Add `hello world` app

---
+++ cyak_cli/src/app/mod.rs
@@ -0,0 +1,15 @@
+use crate::cli::Cli;
+
+pub fn run(cli: Cli) -> anyhow::Result<()> {
+    let mut siv = cursive::default();
+
+    siv.add_global_callback('q', |s| s.quit());
+
+    siv.add_layer(cursive::views::TextView::new(
+        "Hello cursive! Press <q> to quit.",
+    ));
+
+    siv.run();
+
+    Ok(())
+}
