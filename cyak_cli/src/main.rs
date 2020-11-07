1ab5cece07fceb8ab957748ed7e59b9922f5615c
LazyMechanic <AsharnRus@gmail.com>
2020-11-07 14:09:24 +0300

    Add log crate

--- cyak_cli/src/main.rs
+++ cyak_cli/src/main.rs
@@ -5,6 +5,7 @@ use cli::Cli;
 
 fn main() {
     let app = || {
+        init_logger();
         let cli = Cli::new()?;
         app::run(cli)
     };
@@ -16,4 +17,23 @@ fn main() {
             1
         }
     });
+}
+
+fn init_logger() {
+    let log_filters = std::env::var("RUST_LOG").unwrap_or("error".to_string());
+
+    env_logger::Builder::new()
+        .parse_filters(&log_filters)
+        .format(|formatter, record| {
+            use std::io::Write;
+
+            writeln!(
+                formatter,
+                "{} [{}]: {}",
+                chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .init();
 }
